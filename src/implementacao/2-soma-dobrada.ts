11493915c4c6dd1a6de5eaa12c6aa72e693705d2
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-17 15:54:59 -0300

    exercicio soma-dobrada resolvido

--- src/implementacao/2-soma-dobrada.ts
+++ src/implementacao/2-soma-dobrada.ts
@@ -1,4 +1,12 @@
-function somaDobrada(umValor: number, outroValor: number) {
-    return null;
+
+function somaDobrada(umValor: number, outroValor: number) : number {
+    
+    if (umValor == outroValor) {
+        return (umValor + outroValor) * 2;
+    } else if (umValor < 1 || outroValor < 1) {
+        return -1;
+    }else {
+        return umValor + outroValor;
+    }
 }
 module.exports = somaDobrada;

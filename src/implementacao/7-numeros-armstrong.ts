2454b9d0987a9988de651f0e1184279f64a2baba
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-18 09:50:40 -0300

    exercicio numeros de armstrong resolvido

--- src/implementacao/7-numeros-armstrong.ts
+++ src/implementacao/7-numeros-armstrong.ts
@@ -1,4 +1,17 @@
 function numerosArmstrong(numero: number) {
-    return null;
+    const numerosString = numero.toString();
+    const numerosQuantidade = numerosString.length; 
+    let soma = 0;
+    
+    for (let i = 0; i < numerosQuantidade; i++) {
+        const digito = parseInt(numerosString[i]);
+        soma += Math.pow(digito, numerosQuantidade);
+    }
+
+    if (soma === numero) {
+        return "Este eh um numero de Armstrong!";
+    } else {
+        return "Este nao eh um numero de Armstrong!";
+    }
 }
 module.exports = numerosArmstrong;
